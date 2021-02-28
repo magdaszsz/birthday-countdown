da6cd9f48c8ff7eda4411f6377f4b932ccef86d2
Magda Sz <pesahson@gmail.com>
2021-02-28 14:57:11 +0100

    countdown working

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const msg = document.querySelector('#message');
 const form = document.querySelector('#form');
 const pas1 = document.querySelector('.password1');
 const pas2 = document.querySelector('password2');
+const daysUi = document.querySelector('.countdown__days');
+const hoursUi = document.querySelector('.countdown__hours');
+const minutesUi = document.querySelector('.countdown__minutes');
+const secondsUi = document.querySelector('.countdown__seconds');
 
 let isValid = false;
 
@@ -35,14 +39,30 @@ let countdownValue = Date;
 function calculateDifference() {
   const now = new Date().getTime();
   const difference = countdownValue - now;
+  console.log(difference);
+  const days = Math.floor( difference / day);
+  const hours = Math.floor((difference % day) / hour);
+  const minutes = Math.floor((difference % hour) / minute);
+  const seconds = Math.floor((difference % minute) / second);
+  console.log(days, hours, minutes, seconds)
+  daysUi.textContent = days;
+  hoursUi.textContent = hours;
+  minutesUi.textContent = minutes;
+  secondsUi.textContent = seconds;
 }
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 function updateCountdown(e) {
   e.preventDefault();
   countdownDate = e.srcElement[0].value;
-  console.log(countdownDate)
+  // console.log(countdownDate)
   countdownValue = new Date(countdownDate).getTime();
-  console.log('countdownValue', countdownValue)
+  // console.log('countdownValue', countdownValue)
+ setInterval(calculateDifference, 1000)
 }
 
 formCountdown.addEventListener("submit", updateCountdown);
